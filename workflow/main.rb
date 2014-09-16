b29cc9f9aac89218eb3a46de8a0eaaae7f4846c0
Parker Moore <parkrmoore@gmail.com>
2014-09-16 14:42:56 -0700

    Update for Alfred 2.

--- workflow/main.rb
+++ workflow/main.rb
@@ -1,35 +1,37 @@
 #!/usr/bin/env ruby
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require "bundle/bundler/setup"
+require "bundler/setup"
 require "alfred"
 
 require 'gemoji'
-require 'emoji_characters'
+require_relative 'emoji_characters'
 
 def icon_hash(emoji)
-  path = File.join(Emoji.images_path, 'emoji', "#{emoji}.png")
+  path = File.join(Emoji.images_path, 'emoji', emoji.image_filename)
   { :type => 'default', :name => path }
 end
 
-def emoji_item(emoji_name)
-  emoji_code = ":#{emoji_name}:"
+def emoji_item(emoji)
+  emoji_code = ":#{emoji.name}:"
   {
     :uid => emoji_code,
     :title => emoji_code,
-    :icon => icon_hash(emoji_name),
-    :arg =>  EmojiCharacters.from_code(emoji_name) || emoji_code
+    :icon => icon_hash(emoji),
+    :arg =>  EmojiCharacters.from_code(emoji.name) || emoji_code
   }
 end
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
-  query = Regexp.escape(ARGV.first)
+  if ARGV.size == 1
+    query = Regexp.escape(ARGV.first)
 
-  items = Emoji.names.grep(/#{query}/).each do |code|
-    fb.add_item(emoji_item(code))
+    items = Emoji.all.map(&:name).grep(/#{query}/).each do |code|
+      fb.add_item(emoji_item(Emoji.find_by_alias(code)))
+    end
+
+    puts fb.to_xml(ARGV)
   end
-
-  puts fb.to_xml(ARGV)
 end
